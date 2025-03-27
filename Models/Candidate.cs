8906b441fd2a57293878f72357dec192fca62e7a
Birand Kılınç <pacmaidanapac@gmail.com>
2025-03-27 13:04:40 +0300

    Course controller is created

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -7,7 +7,7 @@ namespace StudentEnroll.Models
         public String? Email{get;set;}=String.Empty;
         public String? FirstName{get;set;}=String.Empty;
         public String? LastName{get;set;}=String.Empty;
-<<<<<<< HEAD
+
         public String? FullName=>$"{FirstName} {LastName?.ToUpper()}";
         public int? Age{get;set;}
         public String? SelectedCourse{get;set;}=String.Empty;
@@ -18,20 +18,7 @@ namespace StudentEnroll.Models
             ApplyAt=DateTime.Now;
         }
 
-=======
-        public int? Age{get;set;}
-        public String? SelectedCourse{get;set;}=String.Empty;
-        public DateTime ApplyAt{get;set;}
->>>>>>> 3a9f40c7e3ba5d3926a445594589b540ec90c713
-        
-        public Candidate()
-        {
-            ApplyAt=DateTime.Now;
-        }
     }
-
-
-
 
 
 
